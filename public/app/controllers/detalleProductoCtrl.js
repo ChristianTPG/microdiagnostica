7268bd4dbec94cee799fb806d16a8ffac2ee8f58
Christian Pisani <christian.tpg@gmail.com>
2017-05-22 21:57:16 -0300

    resolved some pupa issues

--- public/app/controllers/detalleProductoCtrl.js
+++ public/app/controllers/detalleProductoCtrl.js
@@ -11,16 +11,6 @@ angular.module('detalleProductoCtrl',['productosData','angularSlideables','boots
         vm.seleccionado;
         vm.descripcionProductoHtml;
 
-        // var $table = $('.table');
-        // var $fixedColumn = $table.clone().insertBefore($table).addClass('fixed-column');
-        //
-        // $fixedColumn.find('th:not(:first-child),td:not(:first-child)').remove();
-        //
-        // $fixedColumn.find('tr').each(function (i, elem) {
-        //     $(this).height($table.find('tr:eq(' + i + ')').height());
-        // });
-
-
         var xpath = '//*[name="'+vm.productos_categoria+'"]//*[name= "'+vm.productos_subcategoria+'"]//*[name= "'+ vm.producto_name +'"]';
 
         $http.get(pathService.path).success(function(data) {
